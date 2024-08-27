7713c77f2c0e603036c5a78a03a53f6337648231
Pa Khan <don@pa-khan.com>
2024-08-27 17:04:33 +0300

    moving files

--- modules/status/index.ts
+++ modules/status/index.ts
@@ -2,13 +2,16 @@ import {ConfigReader} from "./src/configReader.ts";
 import {GenGo} from "./src/genGo.ts";
 import {GenTs} from "./src/genTs.ts.ts";
 import {GenDart} from "./src/genDart.ts";
+import * as path from "node:path";
 
 const configReader = new ConfigReader("./config.yml");
 await configReader.parse()
 
-const genGo = new GenGo(configReader.config, __dirname);
-const genTs = new GenTs(configReader.config, __dirname);
-const genDart = new GenDart(configReader.config, __dirname);
+const outPath = path.join(__dirname, "../../dist/")
+
+const genGo = new GenGo(configReader.config, outPath);
+const genTs = new GenTs(configReader.config, outPath);
+const genDart = new GenDart(configReader.config, outPath);
 genGo.build()
 genTs.build()
 genDart.build()
